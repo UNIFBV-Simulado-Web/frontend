843b85ccbcae6ae5ef5655350f6ca30179fb588b
Diego Saggioro <diegosaggiorobrito@gmail.com>
2025-06-09 05:20:00 -0300

    .

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -24,8 +24,9 @@ const Quiz: React.FC = () => {
   }, []);
 
   return (
-    <div
-      className="relative
+    <Suspense>
+      <div
+        className="relative
       min-h-screen
       w-full
       bg-[#0C222A]
@@ -36,56 +37,55 @@ const Quiz: React.FC = () => {
       after:inset-0
       after:bg-[radial-gradient(circle_500px_at_bottom_right,_rgba(22,78,99,0.5),_transparent_80%)]
       flex flex-col items-center"
-    >
-      <header className="w-full p-4 sm:p-6 lg:p-8 font-sans">
-        <div className="w-full flex justify-between items-center">
-          <h1 className="text-2xl font-bold">Quiz Aprende+</h1>
-          <nav>
-            <div className="flex items-center gap-x-6">
-              <Link
-                href="/"
-                className="font-sans font-medium text-white hover:text-gray-900"
-              >
-                Menu
-              </Link>
-              <Link
-                href="#"
-                className="font-sans font-medium text-white hover:text-gray-900"
-              >
-                Login
-              </Link>
-              <Link
-                href="#"
-                className="rounded-md border transparent px-4 py-2 font-semibold text-white shadow-sm hover:bg-gray-700 transition-colors"
-              >
-                Cadastre-se
-              </Link>
-            </div>
-          </nav>
-        </div>
-      </header>
-      <div className="w-11/12 h-px bg-white/50 mb-8"></div>
+      >
+        <header className="w-full p-4 sm:p-6 lg:p-8 font-sans">
+          <div className="w-full flex justify-between items-center">
+            <h1 className="text-2xl font-bold">Quiz Aprende+</h1>
+            <nav>
+              <div className="flex items-center gap-x-6">
+                <Link
+                  href="/"
+                  className="font-sans font-medium text-white hover:text-gray-900"
+                >
+                  Menu
+                </Link>
+                <Link
+                  href="#"
+                  className="font-sans font-medium text-white hover:text-gray-900"
+                >
+                  Login
+                </Link>
+                <Link
+                  href="#"
+                  className="rounded-md border transparent px-4 py-2 font-semibold text-white shadow-sm hover:bg-gray-700 transition-colors"
+                >
+                  Cadastre-se
+                </Link>
+              </div>
+            </nav>
+          </div>
+        </header>
+        <div className="w-11/12 h-px bg-white/50 mb-8"></div>
 
-      <div className="relative w-full z-10 flex-grow">
-        {questions.length === 0 ? (
-          <p>Nenhuma questão encontrada</p>
-        ) : (
-          <Suspense fallback={<div>A preparar o seu quiz...</div>}>
+        <div className="relative w-full z-10 flex-grow">
+          {questions.length === 0 ? (
+            <p>Nenhuma questão encontrada</p>
+          ) : (
             <Question question={getCurrentQuestion() as QuestionType} />
-          </Suspense>
-        )}
-      </div>
+          )}
+        </div>
 
-      <footer className="w-full py-6 text-center">
-        <p className="text-sm text-gray-400">
-          <span>© 2025 Quiz Aprende+</span>
-          <span className="mx-2">|</span>
-          <span>Projeto acadêmico-extensivo</span>
-          <span className="mx-2">|</span>
-          <span>Wyden-UniFBV</span>
-        </p>
-      </footer>
-    </div>
+        <footer className="w-full py-6 text-center">
+          <p className="text-sm text-gray-400">
+            <span>© 2025 Quiz Aprende+</span>
+            <span className="mx-2">|</span>
+            <span>Projeto acadêmico-extensivo</span>
+            <span className="mx-2">|</span>
+            <span>Wyden-UniFBV</span>
+          </p>
+        </footer>
+      </div>
+    </Suspense>
   );
 };
 export default Quiz;
